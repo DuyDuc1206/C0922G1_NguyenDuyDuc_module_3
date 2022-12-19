f53446cdc229084e77d6c389cd7b76be9e3d91d6
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-12-19 10:43:17 +0700

    update

---
+++ Module 3/ss4_ham_thong_dung/ham_thong_dung.sql
@@ -0,0 +1,42 @@
+create database if not exists ham_thong_dung;
+drop 
+  database if exists ham_thong_dung;
+use quan_ly_sinh_vien;
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select 
+  * 
+from 
+  `subject` 
+where 
+  Credit = (
+    select 
+      max(Credit) 
+    from 
+      `subject`
+  );
+  
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select 
+  s.*, 
+  m.Mark 
+from 
+  `subject` as s 
+  join mark as m on s.SubId = m.SubId 
+where 
+  m.Mark = (
+    select 
+      max(Mark) 
+    from 
+      mark
+  );
+  
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select 
+  st.*, 
+  avg(m.Mark) 
+from 
+  student as st 
+  join mark as m on st.StudentId = m.StudentId 
+group by 
+  m.studentId;
