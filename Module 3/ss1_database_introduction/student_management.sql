9fd7dd99de8720b21f58474833c6c13a2e580973
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-12-20 15:20:01 +0700

    update

--- Module 3/ss1_database_introduction/student_management.sql
+++ Module 3/ss1_database_introduction/student_management.sql
@@ -31,7 +31,7 @@ CREATE TABLE mark
     mark_id    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     sub_id     INT NOT NULL,
     student_id INT NOT NULL,
-    mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    mark      FLOAT   DEFAULT 0 CHECK ( mark BETWEEN 0 AND 100),
     exam_times TINYINT DEFAULT 1,
     UNIQUE (sub_id, student_id),
     FOREIGN KEY (sub_id) REFERENCES subject (sub_id),
