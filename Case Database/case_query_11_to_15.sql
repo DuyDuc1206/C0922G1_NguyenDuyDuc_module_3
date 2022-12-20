e05ba2a5e469f96cfad2369f7606351ebc9331a1
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-12-20 15:42:16 +0700

    update

--- Case Database/case_query_11_to_15.sql
+++ Case Database/case_query_11_to_15.sql
@@ -1,7 +1,8 @@
 create database if not exists case_query_11_to_15;
-drop database if exists case_query_11_to_15;
+drop 
+  database if exists case_query_11_to_15;
 use case_study_database;
-  
+
 -- task 11.  Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng 
 -- bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
 select 
@@ -29,7 +30,10 @@ select
   kh.ho_ten, 
   kh.so_dien_thoai, 
   dv.ten_dich_vu, 
-  ifnull(sum(hdct.so_luong),0) as so_luong, 
+  ifnull(
+    sum(hdct.so_luong), 
+    0
+  ) as so_luong, 
   hd.tien_dat_coc 
 from 
   hop_dong as hd 
@@ -42,39 +46,77 @@ where
     month(hd.ngay_lam_hop_dong) in (10, 11, 12)
   ) 
   and year(hd.ngay_lam_hop_dong) = 2020 
-  and hd.ma_dich_vu not in 
-  (
-  select hd.ma_dich_vu 
-  from hop_dong as hd 
-  join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu
-  where ( quarter(ngay_lam_hop_dong) = 1 or quarter(ngay_lam_hop_dong) = 2 ) and year(ngay_lam_hop_dong) = 2021
-  )
+  and hd.ma_dich_vu not in (
+    select 
+      hd.ma_dich_vu 
+    from 
+      hop_dong as hd 
+      join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu 
+    where 
+      (
+        quarter(ngay_lam_hop_dong) = 1 
+        or quarter(ngay_lam_hop_dong) = 2
+      ) 
+      and year(ngay_lam_hop_dong) = 2021
+  ) 
 group by 
   hdct.ma_hop_dong;
   
-  -- task 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
-  -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
-  select dvdk.*,
-  sum(hdct.so_luong) as'so_luong'
-  from dich_vu_di_kem as dvdk 
-  join hop_dong_chi_tiet as hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
-  group by ma_dich_vu_di_kem
-  having sum(hdct.so_luong) >= all 
-  ( select sum(hdct.so_luong) 
-  from hop_dong_chi_tiet as hdct 
-  group by ma_dich_vu_di_kem );
+-- task 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select 
+  dvdk.*, 
+  sum(hdct.so_luong) as 'so_luong' 
+from 
+  dich_vu_di_kem as dvdk 
+  join hop_dong_chi_tiet as hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem 
+group by 
+  ma_dich_vu_di_kem 
+having 
+  sum(hdct.so_luong) >= all (
+    select 
+      sum(hdct.so_luong) 
+    from 
+      hop_dong_chi_tiet as hdct 
+    group by 
+      ma_dich_vu_di_kem
+  );
   
-  -- task 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
-  -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
-  select hd.ma_hop_dong,
-  ldv.ten_loai_dich_vu,
-  dvdk.ten_dich_di_kem,
-  count(hdct.ma_dich_vu_di_kem)
-  from dich_vu_di_kem as dvdk
-  left join hop_dong_chi_tiet as hdct
-  left join hop_dong as hd
-  left join dich_vu as dv
-  left join loai_dich_vu as ldv
+-- task 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select 
+  hd.ma_hop_dong, 
+  ldv.ten_loai_dich_vu, 
+  dvdk.ten_dich_vu_di_kem, 
+  count(hdct.ma_dich_vu_di_kem) as so_luong 
+from 
+  dich_vu_di_kem as dvdk 
+  left join hop_dong_chi_tiet as hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem 
+  left join hop_dong as hd on hd.ma_hop_dong = hdct.ma_hop_dong 
+  left join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu 
+  left join loai_dich_vu as ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu 
+group by 
+  hdct.ma_dich_vu_di_kem 
+having 
+  count(hdct.ma_dich_vu_di_kem) = 1;
   
-  
-  
+-- task 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi 
+-- mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select 
+  nv.ma_nhan_vien, 
+  nv.ho_va_ten, 
+  td.ten_trinh_do, 
+  bp.ten_bo_phan, 
+  nv.so_dien_thoai, 
+  nv.dia_chi 
+from 
+  nhan_vien as nv 
+  left join trinh_do as td on td.ma_trinh_do = nv.ma_trinh_do 
+  left join bo_phan as bp on bp.ma_bo_phan = nv.ma_bo_phan 
+  left join hop_dong as hd on hd.ma_nhan_vien = nv.ma_nhan_vien 
+where 
+  year(ngay_lam_hop_dong) in (2020, 2021) 
+group by 
+  hd.ma_nhan_vien 
+having 
+  count(hd.ma_nhan_vien) <= 3;
---
+++ Case Database/case_query_16_to_20.sql
@@ -0,0 +1,3 @@
+create database if not exists case_query_16_to_20;
+drop database if exists case_query_16_to_20;
+use case_study_database;
