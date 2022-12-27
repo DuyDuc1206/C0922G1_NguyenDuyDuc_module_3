e0545513c51bbd3640edaabea013259ea2853bff
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-12-27 19:52:14 +0700

    update

--- Case_Web/src/main/java/service/customer/impl/CustomerService.java
+++ Case_Web/src/main/java/service/customer/impl/CustomerService.java
@@ -1,9 +1,11 @@
 package service.customer.impl;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 import model.Customer;
 import repository.customer.ICustomerRepository;
 import repository.customer.impl.CustomerRepository;
 import service.customer.ICustomerService;
+import validation.RegexCheck;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.Map;
 
 public class CustomerService implements ICustomerService {
     private ICustomerRepository customerRepository = new CustomerRepository();
+
     @Override
     public List<Customer> selectAllCustomer() {
         return customerRepository.selectAllCustomer();
@@ -33,9 +36,41 @@ public class CustomerService implements ICustomerService {
 
     @Override
     public Map<String, String> insertCustomer(Customer customer) {
-        Map<String,String> errorMap = new HashMap<>();
-        if("".equals(customer.getName())){
-            errorMap.put("name","Name is required");
+        Map<String, String> errorMap = new HashMap<>();
+        if ("".equals(customer.getName())) {
+            errorMap.put("name", "Name is required");
+        } else if (!RegexCheck.checkName(customer.getName())) {
+            errorMap.put("name", "Name doesn't match");
+        }
+        if ("".equals(customer.getDateOfBirth())) {
+            errorMap.put("dateOfBirth", "Date of birth is quired");
+        }
+        if ("".equals(customer.getGender())) {
+            errorMap.put("gender", "Gender is required");
+        }
+        if ("".equals(customer.getIdCard())) {
+            errorMap.put("idCard", "ID card is required");
+        } else if (!RegexCheck.checkIdCard(customer.getIdCard())) {
+            errorMap.put("idCard", "ID card doesn't match");
+        }
+        if("".equals(customer.getPhoneNumber())){
+            errorMap.put("phoneNumber","Phone number is required");
+        } else if(!RegexCheck.checkPhoneNumber(customer.getPhoneNumber())){
+            errorMap.put("phoneNumber","Phone number doesn't match");
+        }
+        if("".equals(customer.getAddress())){
+            errorMap.put("address","Address is required");
+        }
+        if ("".equals(customer.getEmail())){
+            errorMap.put("email","Email is required");
+        } else if (!RegexCheck.checkEmail(customer.getEmail())){
+            errorMap.put("email","Email doesn't match");
+        }
+        if("".equals(customer.getCustomerType().getName())){
+            errorMap.put("cusotmerTypeName","Cusmtomer type name is required");
+        }
+        if (errorMap.isEmpty()){
+            customerRepository.insertCustomer(customer);
         }
         return errorMap;
     }
